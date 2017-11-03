1a37599dcbef087f644b1d97c9a013b4b6d1f452
Tran Cong Trinh <congtrinh097@gmail.com>
2017-11-03 10:04:53 +0700

    Update Login UI
    Update

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Form, Item, Input, Button, Text, Left,Right, Icon,Body, Title } from 'native-base';
-import {View} from  'react-native'
+import { StyleSheet ,View, Image} from 'react-native'
+
+const drawerCover = require("../../../img/login_background.jpg");
 export class Login extends Component {
   render() {
     return (
@@ -12,19 +14,53 @@ export class Login extends Component {
         </Body>
         <Right/>
       </Header>
-      <View style={{ padding: 10}}>
-            <Item>
-              <Input  placeholder="Tên đăng nhập" />
+        <Image source={drawerCover} style={styles.background}>
+          <View style={{padding: 10}}>
+            <Item style={styles.button}>
+              <Input placeholder="Tên đăng nhập"/>
             </Item>
-            <Item last>
-              <Input  placeholder="mật khẩu" />
+            <Item last style={styles.button}>
+              <Input placeholder="Mật khẩu"/>
             </Item>
 
-          <Button block style={{ margin: 15, marginTop: 50 }}  onPress={() => this.props.navigation.navigate("Home")}>
-            <Text>Đăng nhập</Text>
-          </Button>
-      </View>
+            <Button block style={styles.button} onPress={() => this.props.navigation.navigate("Home")}>
+              <Left>
+                <Icon active name="person" style={{color: "#fafcfe", marginLeft: 10}}/>
+              </Left>
+              <Text>Đăng nhập</Text>
+              <Right/>
+            </Button>
+
+            <Button info block style={styles.button} onPress={() => this.props.navigation.navigate("Home")}>
+              <Left>
+                <Icon name="logo-facebook" style={{color: "#46639E", marginLeft: 10}}/>
+              </Left>
+              <Text>Đăng nhập bằng facebook</Text>
+              <Right/>
+            </Button>
+
+            <Button danger block style={styles.button} onPress={() => this.props.navigation.navigate("Login")}>
+              <Left>
+                <Icon name="logo-google" style={{color: "#f5e615", marginLeft: 10}}/>
+              </Left>
+              <Text>Đăng nhập bằng google</Text>
+              <Right/>
+            </Button>
+          </View>
+        </Image>
       </Container>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    // remove width and height to override fixed static size
+    width: null,
+    height: null,
+  },
+  button:{
+    margin: 15, marginTop: 20
+  }
+});
